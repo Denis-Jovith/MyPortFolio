004d789a52500087514666d021750e071ba43865
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-01-03 01:27:32 +0300

    I added the ContacFormScreen.kt and used TextField and Button

--- app/src/main/java/com/example/denis_jovitus_buberwa_portifolio_app/ContactFormScreen.kt
+++ app/src/main/java/com/example/denis_jovitus_buberwa_portifolio_app/ContactFormScreen.kt
@@ -1,16 +1,49 @@
 package com.example.denis_jovitus_buberwa_portifolio_app
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ContactFormScreen(){
     Column (modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        Text(text = "Contact Me")
+        Text(text = "Contact Me", fontSize = 24.sp)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Name")},
+            modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+        )
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = {Text("Email")},
+            modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+        )
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = {Text("Message")},
+            modifier = Modifier.fillMaxWidth().padding(bottom = 16.dp)
+        )
+
+        Button(onClick ={}) {
+            Text(text = "Send Message")
+        }
     }
 }
