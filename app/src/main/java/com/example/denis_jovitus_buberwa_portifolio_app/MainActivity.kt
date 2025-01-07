2b92246997777a225986e9d32cdc06c48d1d8dc8
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-01-07 22:31:46 +0300

    Edited the MainActivity

--- app/src/main/java/com/example/denis_jovitus_buberwa_portifolio_app/MainActivity.kt
+++ app/src/main/java/com/example/denis_jovitus_buberwa_portifolio_app/MainActivity.kt
@@ -4,24 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.example.denis_jovitus_buberwa_portifolio_app.ui.theme.Denis_Jovitus_Buberwa_Portifolio_AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             Denis_Jovitus_Buberwa_Portifolio_AppTheme {
-                Surface () {
-
+                Surface (
+                    modifier = Modifier.fillMaxSize()
+                        .windowInsetsPadding(WindowInsets.statusBars)
+                ) {
+                    ContactFormScreen()
                 }
             }
         }
