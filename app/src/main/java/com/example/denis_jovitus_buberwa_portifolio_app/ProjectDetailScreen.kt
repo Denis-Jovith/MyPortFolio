07119a8e61ca33db9c120a9f0df12bfd7f1cd77c
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-01-07 22:32:59 +0300

    Added the ProjectDetailScreen

--- app/src/main/java/com/example/denis_jovitus_buberwa_portifolio_app/ProjectDetailScreen.kt
+++ app/src/main/java/com/example/denis_jovitus_buberwa_portifolio_app/ProjectDetailScreen.kt
@@ -3,10 +3,13 @@ package com.example.denis_jovitus_buberwa_portifolio_app
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,6 +23,8 @@ fun ProjectDetailScreen(projectId : String){
     Column (modifier = Modifier
         .fillMaxSize()
         .padding(16.dp)
+        .windowInsetsPadding(WindowInsets.statusBars)
+
     ) {
         Text(text="Project: $projectId", fontSize = 24.sp, fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.height(16.dp))
