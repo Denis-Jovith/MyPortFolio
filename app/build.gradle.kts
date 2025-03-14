f6c1337865077a6c580d214fb40199b79d516da1
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-03-14 16:43:20 +0300

    modified the file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,8 +56,6 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.constraintlayout.compose.android)
-    implementation(libs.androidx.foundation.android)
-    implementation(libs.androidx.foundation.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
